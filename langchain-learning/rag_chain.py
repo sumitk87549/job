c014e0ca68b58acbdc256065c334d969f796f2cd
Sumit Kumar <sumitk87549@gmail.com>
2026-06-06 16:56:40 +0530

    .

--- langchain-learning/rag_chain.py
+++ langchain-learning/rag_chain.py
@@ -12,26 +12,28 @@ from langchain_groq import ChatGroq
 
 book = PyPDFLoader("./books/AOSH.pdf").load()
 
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+embedding_object = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 print("embeddings created")
-vectors = Chroma.from_documents(documents=book, embedding=embeddings)
-print("Vectors created")
 
-splitter = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=60)
+split_chunks = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=60).split_documents(book)
+vectors = Chroma.from_documents(documents=split_chunks, embedding=embedding_object)
+print("Vectors created")
 retiever = vectors.as_retriever(search_kwargs={"k":3})
 print("Vectors retrieved")
+
 prompt=ChatPromptTemplate.from_template("Answer the question based on the following context [GIVE ANSWER ONLY]: \n {context} \n Question: {question}")
 print("Prompt created")
 
 parser = StrOutputParser()
 model = ChatGroq(model='llama-3.1-8b-instant')
-question = input("\nAsk me : ")
 chain = prompt | model | parser 
 
 
 # ***********NEW***********
-retrieved_docs = retiever.invoke(question)
-context = '\n\n'.join([doc.page_content for doc in retrieved_docs])
+retrieved_chunks = retiever.invoke(question)
+context = '\n\n'.join([doc.page_content for doc in retrieved_chunks])
+
+question = input("\nAsk me : ")
 response = chain.invoke({"context": context, "question": question})
 print("LLM working...")
 
