6452db63bcb60ecad1c47c6165f346cd1008c995
Sumit Kumar <sumitk87549@gmail.com>
2026-06-29 18:55:11 +0530

    .

---
+++ RAG-Full/backend/app/services/normalizers/text_normalizer.py
@@ -0,0 +1,22 @@
+class TextNormalizer:
+    
+    @staticmethod
+    def normalize():
+        pass
+
+    def _normalize_line_endings():
+        pass
+    
+    def _remove_control_characters():
+        pass
+    
+    def _strip_trailing_spaces():
+        pass
+    
+    def _trim_document():
+        pass
+
+    def _collapse_blank_lines():
+        pass
+    
+    
